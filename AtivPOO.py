abe68a869dbf412944584be6a3ea3cf7eb715931
alinelima30 <alimary1994@gmail.com>
2024-10-15 11:44:30 -0300

    init commit

--- AtivPOO.py
+++ AtivPOO.py
@@ -5,11 +5,30 @@ p1.comer()
 p1.andar()
 """
 
-"""from Biblioteca import *
+from Biblioteca import *
 p1 = Animal("Leão","Branco com pintas marrons")
-p1.comer()"""
+p1.comer()
 
-"""from Biblioteca import *
-a1 = Gato("Gato", "Pretinho")
+from Biblioteca import *
+a1 = Gato("Mimi", "Pretinho")
+a1.miar()
+
+#complete este exercício sobre
+# herança, criando as classes cachorro, coelho e vaca, herdando da Classe Animal. +(polimorfismo - comer)
+
+from Biblioteca import *
+a1 = Cachorro("Sansão", "Black")
 a1.comer()
-a1.miar() """
+a1.latir()
+
+from Biblioteca import *
+a1 = Coelho("Ig", "Malhado")
+a1.chiar()
+
+from Biblioteca import *
+c1 = Corredor("Joy", 65)
+c1.aposentar()
+c1.aquecer()
+c1.correr()
+t1 = TriAtleta("Joy", 22)
+t1.nadar()
