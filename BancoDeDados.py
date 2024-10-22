0aeb0df2731f61e8765767b9ec003d9b9f6ad435
alinelima30 <alimary1994@gmail.com>
2024-10-22 09:53:04 -0300

    init commit

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -1,5 +1,9 @@
-#Dado X Informação: Dado é a menor parte de um banco de dado,
+#Dado: Dado é a menor parte de um banco de dado,
 #é o elemento bruto geralmente fornecido por usuários ou por suas ações e, por si só não quer dizer nada.
+
+#Informação: As informações são os dados devidamente tratados e analisados, produzindo conhecimento relevante.
+#Ao contrário dos elementos brutos do tópico anterior, elas têm significados práticos e podem ser utilizadas
+#para reforçar o processo de tomada de decisão.
 
 #Base de dados: Um conjuntos de arquivos relacionados entre si.
 
@@ -7,3 +11,16 @@
 #onde estão armazenados os dados de diversos sistemas, para consulta e atualização pelo usuário.
 
 #Dado: É o valor do campo quando é armazenado no Banco de Dados.
+
+# - Modelo conceitual: (Visão geral de negócio, facilidade de entendimento entre usuário e desenvolvedores);
+
+# - Modelo lógico: (derivado do modelo conceitual, define as chaves primárias das entidades, as entidades e
+#atributos são documentados em um Dicionário de Dados, define chaves estrangeiras entre as entidades,
+#adequado padrão de nomenclatura adotado pela empresa) Banco de dados hierárquico, banco de dados de rede,
+#banco de dados relacional, etc...
+
+# - Modelo físico: elaborado a partir do modelo lógico, pode variar segundo a tecnologia usada para
+#implementação do banco de dados, possui tabelas físicas(log, lider, etc..),
+#possui colunas físicas(replicação), a linguagem sql é a linguagem padrão para definição, manipulação e
+#controle de uso das estruturas de dados.) "(DBA-Data Based adminisrtrative) - administrador do banco de dados
+#sendo especialista em um tipo de estrutura".
