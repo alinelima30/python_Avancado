7fda0557791339724b825b0c250269e181aeeb6d
alinelima30 <alimary1994@gmail.com>
2024-11-05 12:37:15 -0300

    init commit

--- biblioteca0511.py
+++ biblioteca0511.py
@@ -1,14 +1,28 @@
 import requests
-def viacep(cep):
-    if len(cep) ==8:
-        link = f'https://viacep.com.br/ws/{cep}/json/'
-        requisicao = requests.get(link)
-        print(requisicao)
-        dic_requisicao = requisicao.json()
+import mysql.connector
+banco = mysql.connector.connect(    #conexão com o banco de dados)
+    host = "localhost",
+    user = "root",
+    password = "123456",
+    database = "turmaa"
+)
+meucursor=banco.cursor()
+cep = input("Qual o cep: ")
+if len(cep) ==8:
+    link = f'https://viacep.com.br/ws/{cep}/json/'
+    requisicao = requests.get(link)
+    print(requisicao)
+    dic_requisicao = requisicao.json()
 
-        uf = dic_requisicao['uf']
-        cidade = dic_requisicao['localidade']
-        bairro = dic_requisicao['bairro']
-        print(dic_requisicao)
-    else:
+    cep = dic_requisicao['cep']
+    logradouro = dic_requisicao['logradouro']
+    complemento = dic_requisicao['complemento']
+
+    sql = "insert into enderecos (cep, logradouro, complemento) values(%s, %s, %s)"
+    data = (cep, logradouro, complemento)
+    meucursor.execute(sql,data)
+    banco.commit()
+    meucursor.close()
+    banco.close()
+else:
         print("CEP Inválido")
