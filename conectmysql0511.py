d5bf19053dc62d3912d716f7fb778c6fff999d28
alinelima30 <alimary1994@gmail.com>
2024-11-05 11:59:37 -0300

    init commit

--- conectmysql0511.py
+++ conectmysql0511.py
@@ -1,17 +1,24 @@
 import mysql.connector
 
-banco = mysql.connector.connect(
+banco = mysql.connector.connect(    #conexão com o banco de dados)
     host = "localhost",
     user = "root",
     password = "123456",
     database = "turmaa"
 )
-meucursor = banco.cursor()
-pesquisa = 'select * from funcionario'
+meucursor = banco.cursor()  #usaa as tabelas do banco de dados)
+#fetchall recebe tudo da pesquisa e retorna através de uma tupla.
+pesquisa = 'select * from alunos'
 meucursor.execute(pesquisa)
 resultado = meucursor.fetchall()
 for x in resultado:
     print(x)
+nome1 = input("Digite o nome: ")
+telefone1 = input("Digite o telefone: ")
+sql = "insert into alunos(nome,telefone) values(%s,%s)"
+data = (nome1,telefone1)
+meucursor.execute(sql,data)
+banco.commit()
 meucursor.close()
 banco.close()
 
