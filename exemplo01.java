fb0c19364549fa0ee1576c44f7c7041e79701aec
Aline Lima <alimary1994@gmail.com>
2024-11-12 11:39:00 -0300

    Add files via upload

---
+++ exemplo01.java
@@ -0,0 +1,13 @@
+package aula01;
+
+public class exemplo01 {
+
+  public static void main(String[] args) {
+	int x = 10;
+	System.out.println (x++); //syso ctrl + barra de espaço(system...)
+	System.out.println (++x);
+	System.out.println (x--);
+	System.out.println (--x); //ctrl + alt + seta pra baixo duplica		
+  }	
+	
+}
